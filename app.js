da26a9df8ebf9d1118fe5ec31aae1bd273ccd1e8
Deibis <adayam85@gmail.com>
2024-10-12 09:41:15 -0500

    Instalación de nodemon

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+app.get ('/',(req,res)=> {
+    res.send('HOLA MUNDO');
+
+})
+
 app.listen(3000, (req, res)=>{
     console.log('SERVER RUNNING IN http://localhost:3000');
 })
