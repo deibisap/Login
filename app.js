a6ddac9af6d8b40deaf6dfd32891c39202c04f4c
Deibis <adayam85@gmail.com>
2024-10-12 11:04:05 -0500

    Conexión a la base de datos satisfactoria

--- app.js
+++ app.js
@@ -1,6 +1,42 @@
+//invocamos express
+
 const express = require('express');
 const app = express();
 
+//2 seteamos urlencoded para capturar los datos del formulario
+app.use(express.urlencoded({extended:false}));
+app.use(express.json()); 
+
+//3 invocamos a dotenv
+const dotenv = require('dotenv');
+dotenv.config({path:'./env/.env'});
+
+
+//4. el directorio public
+app.use('/resources', express.static('public'));
+app.use('/resources', express.static(__dirname + '/public'));
+
+// 5 establecer motor de plantillas
+app.set('view engine', 'ejs');
+
+//6 invocanos a bcryptjs
+const bcryptjs = require('bcryptjs');
+
+//7 Variable de session
+const session = require('express-session');
+app.use(session({
+        secret:'secret',
+        resave: true,
+        saveUninitialized: true
+}));
+ 
+
+//8 invocar modulo de base de datos
+
+const connection = require('./database/db');
+
+
+console.log(__dirname);
 app.get ('/',(req,res)=> {
     res.send('HOLA MUNDO');
 
@@ -8,4 +44,4 @@ app.get ('/',(req,res)=> {
 
 app.listen(3000, (req, res)=>{
     console.log('SERVER RUNNING IN http://localhost:3000');
-})
+}) 
